c2b8989ec29ec601dd01be43aee8ce455691bae1
chessman <chessman@world.com>
2024-05-29 09:04:22 +0200

    Updated app.js

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 import express from "express"
 import bodyParser from "body-parser";
 import pg from "pg";
+import { body, query, validationResult } from "express-validator";
 
 const app = express();
 const port = 3000;
 
 app.use(express.static("public"))
 app.use(bodyParser.urlencoded({extended:true}))
+app.set("view engine", "ejs")
 
 app.get("/", (req, res) => {
     res.render("index.ejs")
@@ -28,9 +30,20 @@ app.get("/booking", (req, res) => {
     res.render("booking.ejs")
 })
 
-// STOPPED HERE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-app.post("/submit", (req, res) => {
-    const {email} = req.body;
+// Add database to manage Email collection
+const validateEmail = () => body("email").optional().notEmpty().escape().trim().isEmail();
+app.post("/submit", validateEmail(), body("email").custom(async value => {
+    const user = await userCollection.findUserByEmail(value)
+    if (user) {
+        throw new Error ("Email is already in use")
+    }
+}), (req, res) => {
+    const result = validationResult(req);
+    if (result.isEmpty()) {
+
+    } else {
+        console.log({errors: result.array()})
+    }
 })
 
 app.listen(port, () => {
